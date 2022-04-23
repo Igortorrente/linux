4bd7f55640fd22d2d9d0aa9ba7fef355fc5b831a
Igor Torrente <igormtorrente@gmail.com>
2022-04-23 13:39:21 -0300

    drm: vkms: Add fb information to `vkms_writeback_job`
    This commit is the groundwork to introduce new formats to the planes and
    writeback buffer. As part of it, a new buffer metadata field is added to
    `vkms_writeback_job`, this metadata is represented by the `vkms_frame_info`
    struct.
    
    Also adds two new function pointers (`line_to_frame_func` and
    `frame_to_line_func`) are defined to handle format conversion
    from/to internal format.
    
    A new internal format(`struct pixel_argb_u16`) is introduced to deal with
    all possible inputs. It consists of 16 bits fields that represent each of
    the channels.
    
    These things will allow us, in the future, to have different compositing
    and wb format types.
    
    V2: Change the code to get the drm_framebuffer reference and not copy its
        contents (Thomas Zimmermann).
    V3: Drop the refcount in the wb code (Thomas Zimmermann).
    V5: Add {wb,plane}_format_transform_func to vkms_writeback_job
        and vkms_plane_state (Pekka Paalanen)
    V6: Improvements to some struct/struct members names (Pekka Paalanen).
        Splits this patch in two (Pekka Paalanen).
    
    Signed-off-by: Igor Torrente <igormtorrente@gmail.com>

--- drivers/gpu/drm/vkms/vkms_drv.h
+++ drivers/gpu/drm/vkms/vkms_drv.h
@@ -22,11 +22,6 @@
 
 #define NUM_OVERLAY_PLANES 8
 
-struct vkms_writeback_job {
-	struct iosys_map map[DRM_FORMAT_MAX_PLANES];
-	struct iosys_map data[DRM_FORMAT_MAX_PLANES];
-};
-
 struct vkms_frame_info {
 	struct drm_framebuffer *fb;
 	struct drm_rect src, dst;
@@ -34,6 +29,29 @@ struct vkms_frame_info {
 	unsigned int offset;
 	unsigned int pitch;
 	unsigned int cpp;
+};
+
+struct pixel_argb_u16 {
+	u16 a, r, g, b;
+};
+
+struct line_buffer {
+	size_t n_pixels;
+	struct pixel_argb_u16 *pixels;
+};
+
+typedef void
+(*line_to_frame_func)(struct vkms_frame_info *frame_info,
+		      const struct line_buffer *buffer, int y);
+
+typedef void
+(*frame_to_line_func)(struct line_buffer *buffer,
+		      const struct vkms_frame_info *frame_info, int y);
+
+struct vkms_writeback_job {
+	struct iosys_map data[DRM_FORMAT_MAX_PLANES];
+	struct vkms_frame_info wb_frame_info;
+	line_to_frame_func wb_write;
 };
 
 /**
@@ -44,6 +62,7 @@ struct vkms_frame_info {
 struct vkms_plane_state {
 	struct drm_shadow_plane_state base;
 	struct vkms_frame_info *frame_info;
+	frame_to_line_func plane_read;
 };
 
 struct vkms_plane {
